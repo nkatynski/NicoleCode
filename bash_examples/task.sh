0a690e807594dceadfe3c3db12e7bccb7be32bff
ed <edkatynski@gmail.com>
2020-06-10 20:25:23 -0400

    Added bash example file and detail .txt file.

---
+++ bash_examples/task.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# The line above starts with a "shebang" and points to the interpreter for this shell script
+
+date	# displays current system date
+cal	# displays current system time
+pwd	# displays current working directory
+ls	# lists contents of current working directory
